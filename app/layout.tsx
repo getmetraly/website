edaca992a8d6c27a8fc8b9f8f8d5d8e9158e6290
Andrey Zubarev <andrey@zubarev.net>
2026-05-09 22:22:16 +0300

    Add Vercel Analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Plus_Jakarta_Sans, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import "./shell-polish.css";
@@ -81,7 +82,10 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
